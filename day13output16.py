c2b550eb7cbacfee53ad11a47b172d08292f3864
Akshayaprasadthummala <121208370+Akshayaprasadthummala@users.noreply.github.com>
2023-01-04 16:51:47 +0530

    Add files via upload

---
+++ day13output16.py
@@ -0,0 +1,8 @@
+##with open(r'D:\pythonpractice02\day13\akshaya.txt')as fo:
+##    temp=fo.read()
+##    print(temp)
+
+fo=open(r'D:\pythonpractice02\day13\akshaya.txt')
+temp=fo.read()
+print(temp)
+fo.close()
